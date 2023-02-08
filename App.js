42e0f85a6e223f9048521d8af8352d1da2748226
xenius <xeni0103@stud.kea.dk>
2023-02-08 10:38:52 +0100

    Marx Forlaget visitkort

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View, Image, Linking } from 'react-native';
 
 export default function App() {
+
+  const incrementCounter = () => {
+    Linking.openURL('https://forlagetmarx.dk/');
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <Image
+      source={require("./assets/Marx_forlag.png")}
+      style={{ width: `100%`, height: 150}}
+      resizeMode="contain"
+      />
+      <Text style={styles.headerText}>Med Forlaget Marx håber vi at fasttømre vores position som en af de mest pålidelige kilder til marxistisk litteratur, både historisk og nutidig.</Text>
+      <Text style={{fontWeight: "bold", textAlign: 'center', marginTop: 10}}>God læsning, kammerater!</Text>
+      <Pressable  style={styles.button} onPress={incrementCounter}>
+        <Text style={styles.text}>Besøg os</Text>
+      </Pressable>
+
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -13,8 +28,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f75959',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headerText: {
+    fontStyle: "italic",
+    padding: 2
+  },
+  button: {
+    marginTop: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  }
 });
