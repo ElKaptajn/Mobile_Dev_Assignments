f02f256f56bc1a53f31dcbee7e07af343cdf5b9b
xenius <xeni0103@stud.kea.dk>
2023-02-08 14:21:17 +0100

    Rød baggrund

--- App.js
+++ App.js
@@ -44,7 +44,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 32,
     borderRadius: 4,
     elevation: 3,
-    backgroundColor: 'black',
+    backgroundColor: 'red',
   },
   text: {
     fontSize: 16,
